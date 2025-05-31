cbd89b877cd9cddf5a3de8e1c4de9e3f6b8b5dc4
Chabelabela <szalai.david100@gmail.com>
2025-05-31 20:48:20 +0200

    first commit

---
+++ src/main/java/net/chabelabela/outerwildsmod/OuterWildsModClient.java
@@ -0,0 +1,10 @@
+package net.chabelabela.outerwildsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OuterWildsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
