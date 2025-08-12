3fac556c1d25c937715db88f299aa14110ab90b8
Chabelabela <szalai.david100@gmail.com>
2025-08-12 19:50:55 +0200

    Starting from scratch. Again...

--- src/main/java/net/chabelabela/outerwildsmod/OuterWildsMod.java
+++ src/main/java/net/chabelabela/outerwildsmod/OuterWildsMod.java
@@ -1,46 +1,12 @@
 package net.chabelabela.outerwildsmod;
 
-import net.chabelabela.outerwildsmod.registry.ModBlocks;
-import net.chabelabela.outerwildsmod.registry.ModItems;
-import net.chabelabela.outerwildsmod.registry.ModWorldGeneration;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.chabelabela.outerwildsmod.command.SpaceCommand;
 
 public class OuterWildsMod implements ModInitializer {
-	public static final String MOD_ID = "outerwilds";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	// Clés de registre pour notre dimension
-	public static final RegistryKey<World> SPACE_WORLD_KEY = RegistryKey.of(
-			RegistryKeys.WORLD,
-			Identifier.of(MOD_ID, "space")
-	);
-
-	public static final RegistryKey<DimensionType> SPACE_DIMENSION_TYPE_KEY = RegistryKey.of(
-			RegistryKeys.DIMENSION_TYPE,
-			Identifier.of(MOD_ID, "space_type")
-	);
-
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initialisation d'Outer Wilds Mod - Exploration de l'espace commence !");
-
-		// Initialisation des registres - ORDRE IMPORTANT !
-		ModWorldGeneration.initialize(); // D'abord les générateurs
-		ModBlocks.initialize();
-		ModItems.initialize();
-
-		// Événements serveur
-		ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-			LOGGER.info("Serveur en cours de démarrage - Préparation de l'espace...");
-		});
+		System.out.println("[OuterWildsMod] Mod initialized");
+		SpaceCommand.register();
 	}
 }
---
+++ src/main/java/net/chabelabela/outerwildsmod/command/SpaceCommand.java
@@ -0,0 +1,29 @@
+package net.chabelabela.outerwildsmod.command;
+
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+import static net.minecraft.server.command.CommandManager.literal;
+
+public final class SpaceCommand {
+
+    public static void register() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(
+                    literal("space").executes(ctx -> {
+                        ServerCommandSource src = ctx.getSource();
+
+                        // Run teleport as the server console
+                        src.getServer().getCommandManager().executeWithPrefix(
+                                src.getServer().getCommandSource(),
+                                "execute in outerwilds:space run tp " + src.getName() + " 0 60 0"
+                        );
+
+                        src.sendFeedback(() -> Text.of("Teleporting to space..."), false);
+                        return 1;
+                    })
+            );
+        });
+    }
+}
