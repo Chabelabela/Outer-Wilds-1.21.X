cb13ce3128063db56c2b36385571ab342a644cfc
Chabelabela <szalai.david100@gmail.com>
2025-08-16 20:06:17 +0200

    PROGRESS!!!

--- src/main/java/net/chabelabela/outerwildsmod/OuterWildsModClient.java
+++ src/main/java/net/chabelabela/outerwildsmod/OuterWildsModClient.java
@@ -2,17 +2,36 @@ package net.chabelabela.outerwildsmod;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 import net.chabelabela.outerwildsmod.registry.dimensionrender.SpaceDimensionEffects;
+import com.mojang.blaze3d.systems.RenderSystem;
+import org.lwjgl.opengl.GL11;
 
-import java.util.Objects;
-
+/**
+ * Client entrypoint: registers the DimensionEffects + a no-op sky renderer for a true black void.
+ */
 public class OuterWildsModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        DimensionRenderingRegistry.registerDimensionEffects(
-                Objects.requireNonNull(Identifier.tryParse("outerwildsmod:space")),
-                new SpaceDimensionEffects()
-        );
+        // Use Identifier.of(...) per project preference
+        Identifier effectsId = Identifier.of("outerwildsmod", "space");
+        RegistryKey<World> worldKey = RegistryKey.of(RegistryKeys.WORLD, Identifier.of("outerwildsmod", "space"));
+
+        // Register DimensionEffects (controls fog color / sky type)
+        DimensionRenderingRegistry.registerDimensionEffects(effectsId, SpaceDimensionEffects.INSTANCE);
+
+        // Register SkyRenderer: clear to black and do nothing else (prevents vanilla sky rendering)
+        DimensionRenderingRegistry.registerSkyRenderer(worldKey, (WorldRenderContext ctx) -> {
+            RenderSystem.clearColor(0f, 0f, 0f, 1f);
+            RenderSystem.clear(GL11.GL_COLOR_BUFFER_BIT, false);
+        });
+
+        // Defensive: block clouds and weather from rendering
+        DimensionRenderingRegistry.registerCloudRenderer(worldKey, (WorldRenderContext ctx) -> { /* no clouds */ });
+        DimensionRenderingRegistry.registerWeatherRenderer(worldKey, (WorldRenderContext ctx) -> { /* no weather */ });
     }
 }
--- src/main/java/net/chabelabela/outerwildsmod/registry/dimensionrender/SpaceDimensionEffects.java
+++ src/main/java/net/chabelabela/outerwildsmod/registry/dimensionrender/SpaceDimensionEffects.java
@@ -3,15 +3,21 @@ package net.chabelabela.outerwildsmod.registry.dimensionrender;
 import net.minecraft.client.render.DimensionEffects;
 import net.minecraft.util.math.Vec3d;
 
-public class SpaceDimensionEffects extends DimensionEffects {
-    public SpaceDimensionEffects() {
-        // Distance: NaN disables fog distance calc, no clouds, no alternate sky
+/**
+ * DimensionEffects that make the sky/fog pitch black and disable built-in sky.
+ */
+public final class SpaceDimensionEffects extends DimensionEffects {
+    public static final SpaceDimensionEffects INSTANCE = new SpaceDimensionEffects();
+
+    private SpaceDimensionEffects() {
+        // Float.NaN cloud height disables cloud logic; SkyType.NONE avoids built-in sky rendering
         super(Float.NaN, false, SkyType.NONE, false, false);
     }
 
     @Override
     public Vec3d adjustFogColor(Vec3d color, float sunHeight) {
-        return new Vec3d(0.0, 0.0, 0.0); // pure black
+        // Pure black fog/sky color
+        return new Vec3d(0.0, 0.0, 0.0);
     }
 
     @Override
